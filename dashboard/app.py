1c3f082566c6d4c12eb99f9525a629a3d3089ef4
Brennan Lehman <blehman@nwmissouri.edu>
2024-04-04 12:54:44 -0500

    updates to file

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,59 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
+
+# From shiny, import just reactive and render
+from shiny import reactive, render
+
+# From shiny.express, import just ui and inputs if needed
+from shiny.express import ui
+
+import random
+from datetime import datetime
+from collections import deque
+import pandas as pd
+import plotly.express as px
+from shinywidgets import render_plotly
+from scipy import stats
+
+# --------------------------------------------
+DEQUE_SIZE: int = 5
+reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
+
+MAX_DEQUE_SIZE = 20
+# --------------------------------------------
+# Initialize a REACTIVE CALC that all display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+# It returns a tuple with everything needed to display the data.
+# Very easy to expand or modify.
+# --------------------------------------------
+
+
+@reactive.calc()
+def reactive_calc_combined():
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic
+    temp = round(random.uniform(0, 70), 1)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_dictionary_entry = {"temp":temp, "timestamp":timestamp}
+
+    # get the deque and append the new entry
+    reactive_value_wrapper.get().append(new_dictionary_entry)
+
+    # Get a snapshot of the current deque for any further processing
+    deque_snapshot = reactive_value_wrapper.get()
+
+    # For Display: Convert deque to DataFrame for display
+    df = pd.DataFrame(impaired.csv)
+
+    # For Display: Get the latest dictionary entry
+    latest_dictionary_entry = new_dictionary_entry
+
+    # Return a tuple with everything we need
+    # Every time we call this function, we'll get all these values
+    return deque_snapshot, df, latest_dictionary_entry
 
